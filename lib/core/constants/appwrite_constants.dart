79b7ea5f07cf549dc691afa0b0f2c3933d3728cf
Efrain-DiazM <efradiaz@gmail.com>
2025-03-31 19:35:47 -0500

    actividad APPWRITE

---
+++ lib/core/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+// 3.29
+class AppwriteConstants {
+  static const String endpoint = 'https://cloud.appwrite.io/v1';
+  static const String projectId = '67eb25e3000f96b98fdf';
+  static const String databaseId = '67eb26ad000687003533';
+  static const String collectionId = '67eb26c700357510aaa4';
+}
